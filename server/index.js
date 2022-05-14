9076dade0591995cf9f3d89d82320e8b5c2a96e1
Ufuk Karamalli <ufukkaramalli@gmail.com>
2022-05-14 23:06:45 +0300

    db connection, .env changed,users & soundkit added

--- server/index.js
+++ server/index.js
@@ -1,22 +1,26 @@
 import express from 'express'
 import cors from 'cors'
-import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
+//Database Connection
+import DBconnection from './config/database.js'
+
 // ROUTES
-import tracks from './routes/tracks.js'
+import trackRoutes from './routes/tracks.js'
+import userRoutes from './routes/users.js'
+import soundkitRoutes from './routes/soundkits.js'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
-dotenv.config()
+dotenv.config({ path: '.env' })
 
-app.use('/tracks', tracks)
+DBconnection()
+
+app.use('/tracks', trackRoutes)
+app.use('/users', userRoutes)
+app.use('/soundkits', soundkitRoutes)
 
 app.listen(parseInt(process.env.MONGO_URI_PORT),() => {
-    mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(console.log("Bağlantı Başarılı"))
-    .catch(err => console.log(err))
+    console.log(`We are live on ${process.env.NODE_ENV} mode on port ${parseInt(process.env.MONGO_URI_PORT)}`)
 })
