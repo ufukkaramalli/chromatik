f9ba2ad8357f172fe72ef26e16a82450c3d9f517
Ufuk Karamalli <ufukkaramalli@gmail.com>
2025-09-06 18:15:27 +0300

    cross-env added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,29 @@
-import 'dotenv/config';
-import App from './app'
+// index.ts
+import dotenv from 'dotenv';
+import App from './app';
 import validateEnv from './utils/validateEnv';
 import TrackController from './controller/track.controller';
-import UserController from './controller/user.controller'
+import UserController from './controller/user.controller';
 import SoundkitController from './controller/soundkit.controller';
+
+// NODE_ENV'e göre doğru .env dosyasını yükle
+const envFile =
+  process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
+dotenv.config({ path: envFile });
+
+console.log(`Running in ${process.env.NODE_ENV} mode`);
 
 validateEnv();
 
 const app = new App(
-    /**
-     * Register Controllers
-     */
-    [
-        new TrackController(),
-        new UserController(),
-        new SoundkitController()
-    ],
-    /**
-     * Register server port number
-     */
-    Number(process.env.PORT));
+  // Register Controllers
+  [
+    new TrackController(),
+    new UserController(),
+    new SoundkitController(),
+  ],
+  // Register server port number
+  Number(process.env.PORT)
+);
 
 app.listen();
