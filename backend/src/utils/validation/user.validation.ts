0089c24c083ffc6b78f4a8958835ae4728e518d4
Ufuk Karamalli <ufukkaramalli@gmail.com>
2025-09-10 19:35:07 +0300

    feat: add update validation schema for soundkit, track, and user

--- backend/src/utils/validation/user.validation.ts
+++ backend/src/utils/validation/user.validation.ts
@@ -1,15 +1,33 @@
 import Joi from 'joi'
 
 const register = Joi.object({
-    name: Joi.string().max(30).required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).required(),
-    isValidPassword: Joi.any().equal(Joi.ref('password')).required().label('Confirm password').options({ messages: { 'any.only': '{{#label}} does not match'} })
-})
+  name: Joi.string().max(30).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+  isValidPassword: Joi.any()
+    .equal(Joi.ref('password'))
+    .required()
+    .label('Confirm password')
+    .options({ messages: { 'any.only': '{{#label}} does not match' } })
+});
 
 const login = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().required()
-})
+  email: Joi.string().email().required(),
+  password: Joi.string().required()
+});
 
-export default { register, login }
+const update = Joi.object({
+  name: Joi.string().max(30).optional(),
+  email: Joi.string().email().optional(),
+  password: Joi.string().min(6).optional(),
+  isValidPassword: Joi.any()
+    .equal(Joi.ref('password'))
+    .optional()
+    .label('Confirm password')
+    .options({ messages: { 'any.only': '{{#label}} does not match' } }),
+  role: Joi.string().valid('producer', 'listener', 'admin').optional(),
+  photoUrl: Joi.string().optional(),
+  language: Joi.string().optional()
+});
+
+export default { register, login, update };
