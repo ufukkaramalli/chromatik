0585219d0519ced9d2bb483f28ef8d5877163b3f
Ufuk Karamalli <ufukkaramalli@gmail.com>
2025-09-10 19:35:07 +0300

    feat: add update validation schema for soundkit, track, and user

--- backend/src/utils/validation/soundkit.validation.ts
+++ backend/src/utils/validation/soundkit.validation.ts
@@ -1,15 +1,23 @@
 import Joi from "joi";
 
 const create = Joi.object({
-    title: Joi.string().required(),
-    description: Joi.string().required(),
-    thumbnailUrl: Joi.string(),
-    url: Joi.string().required(),
-    userId: Joi.string().required()
-})
+  title: Joi.string().required(),
+  description: Joi.string().required(),
+  thumbnailUrl: Joi.string(),
+  url: Joi.string().required(),
+  userId: Joi.string().required()
+});
+
+const update = Joi.object({
+  title: Joi.string().optional(),
+  description: Joi.string().optional(),
+  thumbnailUrl: Joi.string().optional(),
+  url: Joi.string().optional(),
+  userId: Joi.string().required()
+});
 
 const deleteSoundkit = Joi.object({
-    userId: Joi.string().required()
-})
+  userId: Joi.string().required()
+});
 
-export default { create, deleteSoundkit }
+export default { create, update, deleteSoundkit };
--- backend/src/utils/validation/track.validation.ts
+++ backend/src/utils/validation/track.validation.ts
@@ -1,8 +1,25 @@
 import Joi from 'joi';
 
 const create = Joi.object({
-    name: Joi.string().required(),
-    url: Joi.string().required()
-})
+  name: Joi.string().required(),
+  url: Joi.string().required(),
+  description: Joi.string().optional(),
+  art: Joi.string().optional(),
+  status: Joi.string().optional(),
+  userId: Joi.string().required()
+});
 
-export default { create }
+const update = Joi.object({
+  name: Joi.string().optional(),
+  url: Joi.string().optional(),
+  description: Joi.string().optional(),
+  art: Joi.string().optional(),
+  status: Joi.string().optional(),
+  userId: Joi.string().required()
+});
+
+const deleteTrack = Joi.object({
+  userId: Joi.string().required()
+});
+
+export default { create, update, deleteTrack };
